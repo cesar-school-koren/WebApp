0e9d7bb114fb2e9ff56bab96c4bc35e93d7c6449
Arthur O. Reis <aor@cesar.school>
2019-05-05 12:33:45 -0300

    add JSP files and servlets

---
+++ src/com/school/koren/controller/Login.java
@@ -0,0 +1,47 @@
+package com.school.koren.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		// aqui fazer a operação de checar no banco de dados
+		try {
+			// operações com banco de dados
+			HttpSession session = request.getSession();
+			session.setAttribute(username, username);
+			response.sendRedirect("homeLoggedIn.jsp");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
