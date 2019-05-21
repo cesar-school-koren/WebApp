6e62c8312be6959f985eeb58c778e6a642706897
ArthurReis <aor@cesar.school>
2019-05-21 11:08:45 -0300

    Update Login.java

--- src/com/school/koren/controller/Login.java
+++ src/com/school/koren/controller/Login.java
@@ -35,7 +35,7 @@ public class Login extends HttpServlet {
     @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 	/**
